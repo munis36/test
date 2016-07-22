d4524b5a295ad71fe21f819deaa2444d957f1421
munis36 <mnagarat@Ammu-MacBook-Pro.local>
2016-07-22 14:04:56 -0400

    installed gulp and gutil

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+/**
+ * Created by mnagarat on 7/22/16.
+ */
+
+var gulp = require('gulp'),
+    gutil = require('gulp-util');
+
+gulp.task('log',function(){
+    gutil.log('workflows are awesome');
+});
